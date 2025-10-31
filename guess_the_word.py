580ea25765da27d0afbf45924019aa3ba3f40169
JOS2302399 <JOS2302399@maricopa.edu>
2025-10-31 12:43:49 -0700

    Added comments to guess_the_word.py

--- guess_the_word.py
+++ guess_the_word.py
@@ -1,14 +1,23 @@
+# Joseph Brekan
+# CIS256
+# EX 4
+
 import random
 
 def word_guessing_game():
+    # Predefined list of words
     word_list = ['sunflower', 'leather', 'monitor', 'tower', 'decoration', 'hunter']
 
+    # Select a random word from the list
     word_to_guess = random.choice(word_list)
 
+    # Create a display version of the word
     guessed_word = ["_"] * len(word_to_guess)
 
+    # Set number of attempts
     attempts = 6
 
+    # Score guessed letters to prevent repeats
     guessed_letters = set()
 
     print('Welcome to the Word Guessing Game!')
@@ -16,9 +25,11 @@ def word_guessing_game():
     print(' '.join(guessed_word))
     print(f'You have {attempts} incorrect attempts allowed.\n')
 
+    # Main game loop
     while attempts > 0 and "_" in guessed_word:
         guess = input("Guess a letter: ").lower()
 
+        # Validate input
         if len(guess) != 1 or not guess.isalpha():
             print('Please enter a single alphabetic letter.\n')
             continue
@@ -28,6 +39,7 @@ def word_guessing_game():
         
         guessed_letters.add(guess)
 
+        # Check if the guessed letter is in the word
         if guess in word_to_guess:
             print('Good guess!\n')
             for i, letter in enumerate(word_to_guess):
@@ -37,14 +49,16 @@ def word_guessing_game():
             attempts -= 1
             print(f'Wrong guess! Attempts left: {attempts}\n')
 
+        # Display current progress
         print(' '.join(guessed_word))
         print()
 
+    # Check win/lose condition
     if '_' not in guessed_word:
         print(f'Congratulations! You guessed the word "{word_to_guess}" correctly!')
     else:
         print(f'Out of attempts! The word was "{word_to_guess}".')
 
+# Run the game
 if __name__ == '__main__':
     word_guessing_game()
-    
